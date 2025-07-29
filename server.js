a6b4e94d5d89b52788bf216aa3ed9188c3690de7
DEEKSHITH RAO <deekshithrao@MacBookPro.lan>
2025-07-29 16:25:45 +0530

    cors update

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json()); 
-app.use(cors());
+app.use(cors({ origin: 'https://atstesting.netlify.app',
+  credentials: true}));
+
 app.use(morgan('dev'));
 
 // Routes
